3a921efdcfb678afb062362c724f73423d5c619c
Mason Powell <slicace1234@gmail.com>
2022-01-25 08:58:42 -0800

    finished tutorial and practiced code coverage

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,13 +1,30 @@
 const mathOperations = require('./calculator');
 
 describe("Calculator tests", () => {
+
   test('adding 1 + 2 should return 3', () => {
     expect(mathOperations.sum(1, 2)).toBe(3);
   });
+
+  test('adding 1 + 5 should return 6', () => {
+    let result = mathOperations.sum(1, 5)
+    expect(result).toBe(6);
+  });
+
+  test('subtracting 2 from 10 should return 8', () => {
+    let result = mathOperations.diff(10, 2);
+    expect(result).toBe(8);
+  })
+
+
+  test('string Matchers',() => {
+    var string1 = 'software testing help';
+
+    expect(string1).toMatch(/ test/);
+
+    expect(string1).toMatch(/abc/)
+  })
+
 })
 
-describe("Calculator fail", () => {
-  test('adding 1 + 3 should return 10', () => {
-    expect(mathOperations.sum(1 + 3).tobe(3));
-  })
-})
+
